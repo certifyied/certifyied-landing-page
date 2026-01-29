db1973d27eb2756253e2ca49605e783d852fc5ad
fawas4093 <fawas4093@gmail.com>
2026-01-29 11:42:44 +0530

    new

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -130,7 +130,7 @@ const Footer = () => {
           transition={{ duration: 0.5, delay: 0.2 }}
         >
           <p className="text-background/50 text-sm">
-            © 2024 Certifyied. All rights reserved.
+            © 2024 Certifyied. All rights reserved. Designed and Developed by Fawas A Shemeer
           </p>
           <div className="flex items-center gap-4">
             {socialLinks.map((social, index) => (
